a42b0fd5d5d106de23db40fa8a94bf3af7fabdca
u9177 <279149270@qq.com>
2018-06-06 14:15:01 +0800

    commit060601

--- BSAnalytics.js
+++ BSAnalytics.js
@@ -11,26 +11,33 @@
     //统一数据发送路由
     var visitorParam = {
         baseUrl: '', //提交到目标服务器的基地址
+        status: 0, //到访状态
         visitorIdentity: '',
         sessionParams: JSON.parse(sessionStorage.getItem('BSAnalyticsParams')),
         //userId, 
         localParams: JSON.parse(sessionStorage.getItem('BSAnalyticsLocalParams'))
     }
 
+    //到访身份验证
     if (visitorParam.sessionParams && visitorParam.localParams) {
         //TODO 不是第一次到访
+        visitorParam.status = 2;
+        //TODO 如果第一次到访，则
     } else if (visitorParam.localParams) {
         //TODO 第一次到访
+        visitorParam.status = 1;
     } else {
         //TODO 其他情况都是异常，也许是因为还没从远程接口获取到配置信息
+        visitorParam.status = 0;
     }
 
     var visitorIdentification = function() {
 
     }
+
     if (visitorParam.localParams) {
 
     }
-    var _thisDoc = thisWindow.document;
 
+    var _thisDoc = thisWindow.document;
 }(window)
